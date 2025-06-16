860525dfb038ad81eda5c618a0cab725daf2d7bc
Oussama1Belkadi <oussama.belkadi@symolia.com>
2025-06-16 15:13:13 +0100

    slight change in custor entity

--- src/main/java/com/example/securityjwt/entity/Customer.java
+++ src/main/java/com/example/securityjwt/entity/Customer.java
@@ -1,12 +1,11 @@
 package com.example.securityjwt.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,4 +19,19 @@ public class Customer {
     private String email;
     private String phone;
     private String address;
+    private String setUpdatedBy;
+    private String createdBy;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
